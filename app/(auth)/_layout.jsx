acb35424adfa2c6ea5b6e9ff7992a7b3a25a2591
TaiJK <86619937+Linh001@users.noreply.github.com>
2024-04-24 00:43:49 +0700

    Revert "new app -> older version fix conflict"
    This reverts commit 49df43ecfa80d194ed6641418e968cc9f7e544bb.

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {StatusBar} from 'expo-status-bar'
+import {Stack} from 'expo-router'
+const AuthLayout = () => {
+  return (
+    <>
+    <Stack>
+      <Stack.Screen 
+      name="sign-in" 
+      options={{
+        headerShown: false
+      }}
+      />
+      <Stack.Screen 
+      name="sign-up" 
+      options={{
+        headerShown: false
+      }}
+      />
+     
+    </Stack>
+    
+    </>
+  )
+}
+
+export default AuthLayout
+
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/history_apply.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const History_apply = () => {
+  return (
+    <View>
+      <Text>History_apply</Text>
+    </View>
+  )
+}
+
+export default History_apply
+
+const styles = StyleSheet.create({})
